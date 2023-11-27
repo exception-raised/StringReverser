47fe4d1fbc97d5ec92968a35e4cb802679751e95
exception-raised <dimmelman@riseup.net>
2023-11-27 17:18:46 +0200

    Clean up

--- Reverser.asm
+++ Reverser.asm
@@ -1,5 +1,4 @@
 section .data
-testString db "thisisnowreversed", 0 
 format db "Char: %s", 10, 0  
 
 ; Maybe push string length onto the stack to perserve its value.
@@ -13,25 +12,19 @@ reverse:
     mov rbp, rsp
     xor r10, r10
     xor r9, r9
-    ;mov r8, rdi
-    ;mov rdi, testString
     call strlen
     mov r11, rax
 
-    ; ; If the string length is 0;
+    ; If the string length is 0;
     test r11, r11
     sub r11, 1 ; str_len - 1
     jz __print
-    ; jmp __loop
 
     __loop:
         cmp r9, r11
         jae  __print
-        ;lea r10, [rdi]
-        ;mov rbx, r10
-        mov al, [rdi + r11] ; Load the last character
-        mov dl, [rdi + r9] ; Load the character to be swapped
-        ;mov byte [r10 + r11], 'r' ; Store the character from r9 at r11
+        mov al, [rdi + r11] ; Load the last character. Pointer j
+        mov dl, [rdi + r9] ; Load the character to be swapped. Pointer i
         mov [rdi + r11], dl
         mov [rdi + r9], al ; Store the character from r11 at r9
         inc r9
@@ -39,9 +32,6 @@ reverse:
         jmp __loop
 
     __print:
-        ;mov rsi, r10
-        ;mov r10, testString
-        ;mov byte [r10], 'r'
         mov rsi, rdi
         mov rdi, format
         call printf
